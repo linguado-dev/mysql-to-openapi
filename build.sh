7b0c10a15b360fe7cba3964c11338ee12f925e2b
Nikolaus Schuetz <nikolauspschuetz@gmail.com>
2023-07-01 17:25:29 -0400

    first draft of the fork, will build and generate or openapi spec.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-#!/bin/env/bash
+#!/usr/bin/env bash
 
-docker build -t "$(basename $(realpath .))" .
+
+HERE="$(pwd)"
+NAME="$(basename ${HERE})"
+
+if [ ! "${NAME}" = "mysql-to-openapi" ]; then echo "Run this script from the mysql-to-openapi project root." && exit 1; fi
+
+docker build -t "${NAME}" .
